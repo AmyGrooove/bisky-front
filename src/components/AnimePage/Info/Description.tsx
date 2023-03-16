476aa308bc9c836806e736ff7e3879027feca7eb
Ivan <vanyablya003@gmail.com>
2023-03-16 03:58:43 +0300

    description add

--- src/components/AnimePage/Info/Description.tsx
+++ src/components/AnimePage/Info/Description.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react'
+
 import styles from './index.module.scss'
 
 interface IDescription {
@@ -5,7 +7,41 @@ interface IDescription {
 }
 
 const Description = ({ description }: IDescription) => {
-  return <div className={styles.description}></div>
+  const [openDesc, setOpenDesc] = useState(false)
+
+  const blockRef = useRef<HTMLHeadingElement>(null)
+  const [descHeight, setDescHeight] = useState(0)
+
+  useEffect(() => {
+    if (blockRef.current) {
+      setDescHeight(blockRef.current.offsetHeight)
+    }
+  }, [descHeight, blockRef])
+
+  return (
+    <div className={styles.description}>
+      <h4 className={styles.description__label}>Описание</h4>
+      <h5
+        ref={blockRef}
+        className={`${styles.description__text} ${
+          descHeight >= 119 &&
+          (openDesc
+            ? styles.description__text_active
+            : styles.description__text_unActive)
+        }`}
+      >
+        {description}
+      </h5>
+      {descHeight >= 119 && (
+        <button
+          className={styles.description__button}
+          onClick={() => setOpenDesc(!openDesc)}
+        >
+          {openDesc ? 'Скрыть' : 'Читать дальше'}
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default Description
