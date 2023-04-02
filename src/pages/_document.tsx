b0cd2042b9c49a649a099b6de6bbf994baa7bf8b
Ivan <vanyablya003@gmail.com>
2023-04-02 18:18:20 +0300

    Metrika & Analytics add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 class MyDocument extends Document {
   render() {
@@ -23,6 +24,36 @@ class MyDocument extends Document {
           />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-HVWD016914"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-HVWD016914');
+            `}
+          </Script>
+
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          
+              ym(93051932, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+              });
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
