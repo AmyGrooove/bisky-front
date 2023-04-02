f45825da558711b13daca2fcc363b6a9c95e2d83
Ivan <vanyablya003@gmail.com>
2023-04-03 00:36:27 +0300

    sitemap add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,6 @@ import Footer from "@/components/Footer"
 import Modal from "@/components/Common/Modal"
 
 const roboto = Roboto({
-  subsets: ["latin"],
   weight: ["400", "700"],
 })
 
--- src/supportingTool/constatns/index.ts
+++ src/supportingTool/constatns/index.ts
@@ -1,5 +1,6 @@
 const API_URL = process.env.API_URL || ""
 const KODIK_API_URL = process.env.KODIK_API_URL || ""
+const SITE_URL = process.env.SITE_URL || "https://bisky.one"
 
 const SHIKI_URL = "https://shikimori.one/"
 const BISKY_WIKI_URL = "https://hunterxhunter.fandom.com/ru/wiki/Бискет_Крюгер"
@@ -9,4 +10,4 @@ const links = [
   { name: "В топе", href: "" },
 ]
 
-export { links, API_URL, KODIK_API_URL, SHIKI_URL, BISKY_WIKI_URL }
+export { links, API_URL, KODIK_API_URL, SHIKI_URL, BISKY_WIKI_URL, SITE_URL }
