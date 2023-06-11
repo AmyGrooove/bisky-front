84a7287158e452c055a3b8a5b58d86f2fca879a1
Bair <73276298+zoxione@users.noreply.github.com>
2023-06-11 21:22:44 +0300

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/nextjs"
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+}
+export default config
