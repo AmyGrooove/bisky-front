d78020b30fc25f835566e9f9d866d1c4eea8c681
AmyGrooove <vanyablya003@gmail.com>
2024-02-25 22:35:08 +0300

    storybook fix

---
+++ .storybook/main.ts
@@ -0,0 +1,14 @@
+import type { StorybookConfig } from "@storybook/nextjs"
+const config: StorybookConfig = {
+  stories: ["*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
+  ],
+  core: { disableTelemetry: true },
+  framework: { name: "@storybook/nextjs", options: {} },
+  docs: { autodocs: "tag" },
+  staticDirs: ["../public"],
+}
+export default config
