6f5c469fee9bedeb17a9d54c67b0dd25a818487d
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-03-09 18:33:12 +0300

    Refactoring: scss, CheckMarkIcon, CheckMark component

--- src/shared/ui/atoms/Checkbox/ui/Checkbox.tsx
+++ src/shared/ui/atoms/Checkbox/ui/Checkbox.tsx
@@ -7,6 +7,7 @@ import st from "./Checkbox.module.scss"
 
 const Checkbox = (props: ICheckboxProps) => {
   const { children, className, kind = "check_mark", ...checkboxProps } = props
+
   return (
     <label className={cn(st.checkbox_wrapper, className)} data-kind={kind}>
       <input type="checkbox" className={st.checkbox} {...checkboxProps} />
