74cf065847ef584661fb442657a5e5311f39f935
AmyGrooove <vanyablya003@gmail.com>
2024-04-02 23:39:26 +0300

    added animePage

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,24 @@
 import { MetadataRoute } from "next"
 
-import { APP_URL } from "@shared/constants/envVariables"
+import { APP_URL, IS_DEVELOPMENT } from "@shared/constants/envVariables"
 
-const robots = (): MetadataRoute.Robots => ({
-  rules: [
-    { userAgent: "*", allow: "/" },
-    { userAgent: "Googlebot", allow: "/" },
-    { userAgent: "Yandex", allow: "/" },
-  ],
-  sitemap: `${APP_URL}/sitemap.xml`,
-})
+const robots = (): MetadataRoute.Robots =>
+  IS_DEVELOPMENT
+    ? {
+        rules: [
+          { userAgent: "*", disallow: "/" },
+          { userAgent: "Googlebot", disallow: "/" },
+          { userAgent: "Yandex", disallow: "/" },
+        ],
+        sitemap: `${APP_URL}/sitemap.xml`,
+      }
+    : {
+        rules: [
+          { userAgent: "*", allow: "/" },
+          { userAgent: "Googlebot", allow: "/" },
+          { userAgent: "Yandex", allow: "/" },
+        ],
+        sitemap: `${APP_URL}/sitemap.xml`,
+      }
 
 export default robots
