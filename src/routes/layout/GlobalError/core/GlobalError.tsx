c43fd6dde13fff7471a6af8c6f2b40a64a6d7f31
AmyGrooove <vanyablya003@gmail.com>
2025-02-21 02:32:27 +0300

    updated icons

--- src/routes/layout/GlobalError/core/GlobalError.tsx
+++ src/routes/layout/GlobalError/core/GlobalError.tsx
@@ -12,36 +12,39 @@ const GlobalError = (props: IGlobalError) => {
   const { reset, errorText } = useGlobalError(props)
 
   return (
-    <div className={st.root}>
-      <div className={st.errorBlock}>
-        <div className={st.label}>
-          <Text className={st.labelText} weight="700">
-            Ошибка
-          </Text>
-          <Text className={st.labelCode} weight="700">
-            500
-          </Text>
+    <>
+      <div className={st.root}>
+        <div className={st.errorBlock}>
+          <div className={st.label}>
+            <Text className={st.labelText} weight="700">
+              Ошибка
+            </Text>
+            <Text className={st.labelCode} weight="700">
+              500
+            </Text>
+          </div>
+          <Text className={st.description}>{errorText}</Text>
+          <Image
+            className={st.image}
+            src="/images/biskyTea.png"
+            width={170}
+            height={170}
+            alt="biskyTea"
+          />
+          <Button onClick={reset} variant="big" className={st.button}>
+            Обновить страницу
+          </Button>
+          <Button
+            onClick={() => redirect('/')}
+            variant="big"
+            className={st.button}
+          >
+            На главную
+          </Button>
         </div>
-        <Text className={st.description}>{errorText}</Text>
-        <Image
-          className={st.image}
-          src="/images/biskyTea.png"
-          width={170}
-          height={170}
-          alt="biskyTea"
-        />
-        <Button onClick={reset} variant="big" className={st.button}>
-          Обновить страницу
-        </Button>
-        <Button
-          onClick={() => redirect('/')}
-          variant="big"
-          className={st.button}
-        >
-          На главную
-        </Button>
       </div>
-    </div>
+      <div className={st.empty} />
+    </>
   )
 }
 
--- src/routes/layout/NotFound/core/NotFound.tsx
+++ src/routes/layout/NotFound/core/NotFound.tsx
@@ -8,36 +8,38 @@ import { Button } from '@shared/ui/molecules/Button'
 
 import st from './NotFound.module.scss'
 
-
 const NotFound = () => {
   return (
-    <div className={st.root}>
-      <div className={st.errorBlock}>
-        <div className={st.label}>
-          <Text className={st.labelText} weight="700">
-            Ошибка
-          </Text>
-          <Text className={st.labelCode} weight="700">
-            404
-          </Text>
+    <>
+      <div className={st.root}>
+        <div className={st.errorBlock}>
+          <div className={st.label}>
+            <Text className={st.labelText} weight="700">
+              Ошибка
+            </Text>
+            <Text className={st.labelCode} weight="700">
+              404
+            </Text>
+          </div>
+          <Text className={st.description}>Страница не найдена</Text>
+          <Image
+            className={st.image}
+            src="/images/biskyTea.png"
+            width={170}
+            height={170}
+            alt="biskyTea"
+          />
+          <Button
+            onClick={() => redirect('/')}
+            variant="big"
+            className={st.button}
+          >
+            На главную
+          </Button>
         </div>
-        <Text className={st.description}>Страница не найдена</Text>
-        <Image
-          className={st.image}
-          src="/images/biskyTea.png"
-          width={170}
-          height={170}
-          alt="biskyTea"
-        />
-        <Button
-          onClick={() => redirect('/')}
-          variant="big"
-          className={st.button}
-        >
-          На главную
-        </Button>
       </div>
-    </div>
+      <div className={st.empty} />
+    </>
   )
 }
 
