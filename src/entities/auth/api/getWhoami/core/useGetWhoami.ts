7ffa2c0722037d628defd1dba5825c4fe72f3a52
AmyGrooove <vanyablya003@gmail.com>
2025-03-21 16:21:44 +0300

    auth fix

--- src/entities/auth/api/getWhoami/core/useGetWhoami.ts
+++ src/entities/auth/api/getWhoami/core/useGetWhoami.ts
@@ -1,16 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { getWhoami } from './getWhoami'
-import { useEffect } from 'react'
 
 const useGetWhoami = () => {
   const query = useQuery({
     queryKey: ['auth', 'whoami'],
     queryFn: ({ signal }) => getWhoami(false, signal),
   })
-
-  useEffect(() => {
-    if (query.error) window.location.reload()
-  }, [query.error])
 
   return query
 }
