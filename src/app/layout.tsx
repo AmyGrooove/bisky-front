272846110a0bfa95f5dd67a13c2d9028fd451d8b
AmyGrooove <vanyablya003@gmail.com>
2025-08-03 03:50:56 +0300

    Очистка старых файлов. Переработка под новый бек (ч2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { META_DATA } from '@shared/static'
 import '@shared/styles/global.scss'
 import { Ubuntu } from 'next/font/google'
 import Head from 'next/head'
+import { ReactNode } from 'react'
 
 export const metadata = META_DATA
 export const viewport: Viewport = {}
@@ -13,30 +14,13 @@ const ubuntu = Ubuntu({
   display: 'swap',
 })
 
-const RootLayout = () => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="ru">
       <Head>
         <meta name="theme-color" content="#dd5480" />
       </Head>
-      <body className={ubuntu.className}>
-        <div
-          style={{
-            display: 'flex',
-            gap: '20px',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-            backgroundColor: '#170e19',
-          }}
-        >
-          <h1 style={{ color: 'white' }}>Сайт в доработке</h1>
-          <p style={{ color: 'white' }}>Мы работаем над улучшением сайта.</p>
-          <p style={{ color: 'white' }}>Пожалуйста, вернитесь позже.</p>
-          <img src="/images/biskyLook.png" alt="" width={200} />
-        </div>
-      </body>
+      <body className={ubuntu.className}>{children}</body>
     </html>
   )
 }
